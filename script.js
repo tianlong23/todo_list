5155f897cb2891e50a0747c7580fa64e51043a4d
tianlong23 <longch23@gmail.com>
2023-02-26 16:18:19 +0800

    finalized version with comments in code, fixed animations

--- script.js
+++ script.js
@@ -1,49 +1,7 @@
-/*
-1) Create HTML and CSS outline of what it should look like - Done
-2) Add item - DONE
-    Create function that takes an input in the search bar and stores it in a variable
-        once a user hits enter or clicks 'add item' the function reads the text.content value of the search bar
-        create a unique ID based on a counter starting at 0, this id will be assigned to the id value and matched up with the rest of the element
-        take this text.content and assign it to a temporary variable for the new task
-        the search bar is then cleared back to its' original value
-    Take variable and create a new main element in an item (i.e. the part with the task) (may need a sub-function for this)
-        create a new container element for the new item, this should have the HTML for an entire item
-            includes the checkbox to the left, the element itself, a delete button, and an edit button
-            for the main element, there should be a variable that inputs the text.content of the searchbar
-            for the checkbox, delete button, and edit button, there needs to be identifiers to match it with the specific item
-    append this whole new element in to the existing list of items
-
-3)Create checkbox functionality (when checkbox is clicked, the item should go from gray, to green, and have a strikethrough the text, when checked again goes away) - DONE
-    search the existing element for an object matching both the class and the ID (example here is first an href for a class, then the ID("a.save#country")), should be query selector
-    remove the existing class
-    add a new class that changes the style to 'finished', as noted in the first line of this part
-    remove existing class for the checkbox, delete, and edit buttons as well, add new class for the green style, but with no strikethrough
-
-4) create the delete button and function - Done
-    upon click:
-        search the existing element for a container object matching the ID 
-            have a popup appear double confirming they would like to delete task, "INSERT TASK NAME", this should be a nested function, if returns TRUE, then delete
-                popup should be a second function that takes in the ID  from the delete task function
-                the delete button should toggle the active class which makes the popup appear
-                upon clicking, "YES", the function returns true 
-            this "TRUE" is taken and deletes the element with element.remove()
-
-
-5) Create the edit function - DONE
-    upon click:
-        search the existing element for an object matching both the class and the ID (example here is first an href for a class, then the ID("a.save#country")), should be query selector
-        create a popup that has an input bar, with buttons that say, confirm, and cancel
-            create a click function so that if the user clicks on 'confirm', the function returns TRUE, if they click cancel, then FALSE
-                if TRUE, take the text.content from the input bar, and set to a temporary value
-                for the element that was found after the search, update the text.content with the temporary values text.content
-                if FALSE, end the function
-                re-apply the styling to remove the popup
-
-6) OPTIONAL make function to animate completed tasks to the bottom and shift the other ones up
-*/
-
 var taskNumber = 0
 
+
+/*Gets the new task name from the input bar and sends the data to thea append task*/
 function getTaskName() {
     addTask = document.getElementById('taskButton');
     addTask.addEventListener('click', function() {
@@ -56,7 +14,6 @@ function getTaskName() {
     addTaskEnter = document.getElementById('taskInput');
     addTaskEnter.addEventListener('keypress', function(e){
         if(e.key === 'Enter') {
-            console.log('ere')
             taskName = document.getElementById('taskInput').value
             taskID = 'task' + taskNumber
             taskNumber = taskNumber + 1;
@@ -67,7 +24,7 @@ function getTaskName() {
 }
 getTaskName()
 
-
+/*takes task name from getTaskName, and puts it into a new set of div elements, appends to location for all tasks, adds animations as well */
 function appendTask(taskID, taskName) {
     var addedTask = document.createElement('div');
     addedTask.setAttribute('class', 'taskContainer');
@@ -92,15 +49,17 @@ function appendTask(taskID, taskName) {
     },400)
 }
 
+/*Adds event listener to all checks and check container, and then triggers function to update the style below for the specific task the user clicked on */
 function checkEventListener() {
     var checkContainer = document.getElementsByClassName('checkContainer');
     for (var i = 0; i<checkContainer.length; i++) {
         if(checkContainer){
             checkContainer[i].addEventListener('click', updateStyleDone, false);
+        }
     }
 }
-    }
 
+/*Adds event listener to all delete buttons, then triggers function to delete specific to the task the user clicked on */
 function deleteEventListener() {
     var deleteContainer = document.getElementsByClassName('delete');
     for (var i = 0; i<deleteContainer.length; i++) {
@@ -110,6 +69,7 @@ function deleteEventListener() {
     }
 }
 
+/*Adds event listener to all edit buttons, then triggers function to edit specific to the task the user clicked on */
 function editEventListener() {
     var editContainer = document.getElementsByClassName('edit');
     for (var i = 0; i<editContainer.length; i++) {
@@ -119,7 +79,7 @@ function editEventListener() {
     }
 }
 
-
+/*Adds classes to update the style of div elements in the task, adds additional classes in the 'else' portion to trigger animations */
 function updateStyleDone() {
     taskID = this.id
     var updatedTask = document.querySelector(`#${this.id}.taskContainer`)
@@ -128,7 +88,6 @@ function updateStyleDone() {
         updatedTask.querySelector(`#${this.id}.task`).classList.add('finished-task')
         updatedTask.querySelector(`#${this.id}.checkContainer`).classList.add('finished-checkContainer')
         updatedTask.querySelector(`#${this.id}.edit`).classList.add('finished-edit')
-        //reorderFinishedTasks(updatedTask);
     } else {
         updatedTask.querySelector(`#${this.id}.task`).classList.remove('finished-task')
         updatedTask.querySelector(`#${this.id}.checkContainer`).classList.remove('finished-checkContainer')
@@ -141,18 +100,11 @@ function updateStyleDone() {
             updatedTask.querySelector(`#${taskID}.checkContainer`).classList.remove('checkGreen')
             updatedTask.querySelector(`#${taskID}.edit`).classList.remove('editGreen')
         },400 )
-        //reorderUnFinishedTasks(updatedTask);
     }
     
 }
 
-//may need to create a new class in CSS, and add and remove it with a new animation
-
-//update unclicked checkbox animation to not flash
-//remove unecessary code
-//refactor as much as possible
-//put on git hub as "done"
-
+/* On click, triggers a popup, which then if the user confirms to delete, the function removes that specific task from the DOM */
 function deleteTask() {
     document.getElementById('popup-1').classList.toggle('active');
     divID = this.id;
@@ -162,70 +114,35 @@ function deleteTask() {
         document.getElementById('popup-1').classList.toggle('active');
         confirmDelete.removeEventListener('click', deleteTaskContainer);
     }
-
     confirmDelete.addEventListener('click', deleteTaskContainer);
-
     var cancelDelete = document.getElementById('popCancel')
     cancelDelete.addEventListener('click', function(){
         document.getElementById('popup-1').classList.remove('active')
 });
 };
 
+/* On click opens popup which asks the user for a new name of the task, upon clicking confirm, passes the new name into the textcontent */
 function editTask() {
-
-            document.getElementById('popup-2').classList.toggle('active');
-            id = this.id
-            var confirm = document.getElementById('popConfirm');
-            confirm.addEventListener('click', function() {
-                var newTaskName = document.getElementById('editInput').value;
-                document.querySelector(`#${id}.task`).textContent = newTaskName
-                document.getElementById('popup-2').classList.remove('active')
-                document.getElementById('editInput').value = "";
-            })
-            confirmEnter = document.getElementById('editInput');
-            confirmEnter.addEventListener('keypress', function(e) {
-                if (e.key === 'Enter') {
-                    var newTaskName = document.getElementById('editInput').value;
-                    document.querySelector(`#${id}.task`).textContent = newTaskName
-                    document.getElementById('popup-2').classList.remove('active')
-                    document.getElementById('editInput').value = "";
-                }
-            })
+    document.getElementById('popup-2').classList.toggle('active');
+    id = this.id
+    var confirm = document.getElementById('popConfirm');
+    confirm.addEventListener('click', function() {
+        var newTaskName = document.getElementById('editInput').value;
+        document.querySelector(`#${id}.task`).textContent = newTaskName
+        document.getElementById('popup-2').classList.remove('active')
+        document.getElementById('editInput').value = "";
+    })
+    confirmEnter = document.getElementById('editInput');
+    confirmEnter.addEventListener('keypress', function(e) {
+        if (e.key === 'Enter') {
+            var newTaskName = document.getElementById('editInput').value;
+            document.querySelector(`#${id}.task`).textContent = newTaskName
+            document.getElementById('popup-2').classList.remove('active')
+            document.getElementById('editInput').value = "";
+        }
+    })
     var cancelEdit = document.getElementById('popCancel2')
     cancelEdit.addEventListener('click', function() {
         document.getElementById('popup-2').classList.remove('active')
     })
-}
-
-
-/*function reorderFinishedTasks(updatedTask) {
-    const container = document.getElementById('overviewContainer');
-    const divTasks = Array.from(container.children);
-    divTasks.sort(function(a,b) {
-    if (a === updatedTask) {
-        return 1;
-    } else if (b === updatedTask) {
-        return - 1;
-    } else {
-        return 0;
-    }
-    });
-    console.log(divTasks);
-    divTasks.forEach(div => container.appendChild(div))
-} */
-
-function reorderUnFinishedTasks(updatedTask) {
-    const container = document.getElementById('overviewContainer');
-    const divTasks = Array.from(container.children);
-    divTasks.sort(function(a,b) {
-    if (a === updatedTask) {
-        return -1;
-    } else if (b === updatedTask) {
-        return 1;
-    } else {
-        return 0;
-    }
-    });
-    console.log(divTasks);
-    divTasks.forEach(div => container.appendChild(div))
 }
